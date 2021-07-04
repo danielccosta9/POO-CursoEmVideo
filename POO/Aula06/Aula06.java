c03837d3a9770ca96c205527968f395caa97bbc2
danielccosta9 <danielccosta9@github.com>
2021-07-04 17:19:30 -0300

    Aula06

---
+++ POO/Aula06/Aula06.java
@@ -0,0 +1,13 @@
+package POO.Aula06;
+
+public class Aula06 {
+    public static void main(String[] args) {
+        ControleRemoto c = new ControleRemoto();
+
+        
+        c.maisVolume();
+        c.play();
+        c.abrirMenu();
+        c.fecharMenu();
+    }
+}
